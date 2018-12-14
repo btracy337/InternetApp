482c58574e9be54d8436cfd83da812d4140f14cd
Bryan Tracy <btracy337@gmail.com>
2018-12-14 09:41:18 -0700

    Added a bunch of stuff

--- InternetApp/Resources/AppDelegate.swift
+++ InternetApp/Resources/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // Use this method to release shared resources, save user da ta, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
@@ -43,6 +43,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController)-> Bool
+    {
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController
+            else{return false}
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? InternetDetailViewController
+            else{return false}
+        return false
+        
     }
 
 
